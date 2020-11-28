560b1fb00871f8f8af93765d2f8ba7a053f7c7da
Michael Lynch <michael.lynch8211@gmail.com>
2020-11-28 05:00:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+
+function writeCards(names) {
+  let i = 0
+  while (i < names.length) {
+    console.log (`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    i++,
+  }
+
+  return names;
+}
+
+writeCards(names)
