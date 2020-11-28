c134b610e2e25b4f0394aec3eb3658896e39239d
Michael Lynch <michael.lynch8211@gmail.com>
2020-11-28 05:05:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 function writeCards(names) {
   let cards = []
   for ( let i = 0; i < names.length; i++) {
-    console.log (`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    console.log (`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
     i++;
   }
 
