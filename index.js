7c80ca2c71a65547e303fc00d1047ba86ab69202
Michael Lynch <michael.lynch8211@gmail.com>
2020-11-28 05:06:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solutions in this file
 
-function writeCards(names) {
+function writeCards(names, event) {
   let cards = []
   for ( let i = 0; i < names.length; i++) {
     console.log (`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
